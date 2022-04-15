04c53d81414cb61b5ea185691cfb58e5b6b158e2
deepak852k1 <deepak852k1@gmail.com>
2022-04-15 15:16:06 +0530

    Header done

--- src/Header.js
+++ src/Header.js
@@ -1,86 +1,66 @@
-import React from 'react';
-import './Header.css';
-import SearchIcon from '@mui/icons-material/Search';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import {Link} from "react-router-dom";
-import { useStateValue } from './StateProvider';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
+
 function Header() {
-  const [{basket ,user}, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+
   const handleAuthenticaton = () => {
     if (user) {
       auth.signOut();
-    }}
+    }
+  }
+
   return (
-    <div className='header'>
+    <div className="header">
       <Link to="/">
-         <img 
-             className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-         />
-        </Link>
-        <div className='header__search'>
-          <input className='header__search__input' type="text" />
-          <SearchIcon className='header__searchicon' />
-
-        </div>
-
-        <div className='header__nav'>
-        <Link to={!user && '/login'}>
-          <div onClick={handleAuthenticaton} className='header__option'>
-            <span className='header__optionLineOne'>
-            Hello {!user ? 'Guest' : user.email}
-            </span>
-
-            <span className='header__optionLinetwo'>
-            {user ? 'Sign Out' : 'Sign In'}
-            </span>
-    
-          </div>
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
       </Link>
-          <div className='header__option'>
 
-          <span className='header__optionLineOne'>
-              Returns
-            </span>
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
 
-            <span className='header__optionLinetwo'>
-              & Orders
-            </span>
-            
+      <div className="header__nav">
+        <Link to={!user && '/login'}>
+          <div onClick={handleAuthenticaton} className="header__option">
+            <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
+            <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
           </div>
+        </Link>
 
-          <div className='header__option'>
-
-          <span className='header__optionLineOne'>
-              Your
-            </span>
-
-            <span className='header__optionLinetwo'>
-              Prime
-            </span>
-            
+        <Link to='/orders'>
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
           </div>
-          <Link to="./Checkout">
+        </Link>
+        
 
-             <div className='header__optionBasket'>
-                <ShoppingBasketIcon/>
-                <span className='header__optionLineTwo header__basketCount' >
-                  {basket?.length}</span>
-             </div>
-           </Link>
-
-         
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
         </div>
 
-
-      
-    
-    
-    
-    
-    
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
