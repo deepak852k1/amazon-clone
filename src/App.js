241a09c68c1ac072f868dcbe1048ee9523906e8b
deepak852k1 <deepak852k1@gmail.com>
2022-04-15 17:29:38 +0530

    Final commit!!!!!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useEffect } from "react";
-import './App.css';
-import Header from './Header';
-import Home from './Home';
-import { BrowserRouter as Router, Switch, Route } 
- from "react-router-dom";
-import Checkout from './Checkout';
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
+import Orders from "./Orders";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+const promise = loadStripe(
+  "pk_test_51KomqrSGTgOmppSAqiHQwt4RnlaNvbZKFFUFNrOR7UatuZ6EuoHjmTW1kToUEnBYnPWXnITlsqkeIApCk8FKab3d00qUPyHNFE"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
+
   useEffect(() => {
+    // will only run once when the app component loads...
+
     auth.onAuthStateChanged((authUser) => {
       console.log("THE USER IS >>> ", authUser);
 
@@ -32,32 +41,36 @@ function App() {
       }
     });
   }, []);
+
   return (
     <Router>
-    <div className="App">
-    
-      <Switch>
-        <Route path="/login">
-         <Login/>
-        </Route>
-      <Route path='/checkout'>
-      <Header/>
-       
-       <Checkout/>
-        </Route>
-      <Route path="/">
-      <Header/>
-       
-        <Home/>
-
-         
-      </Route>
-      </Switch>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
       </div>
-      </Router>
+    </Router>
   );
 }
-  
-
 
 export default App;
