8b2dbe26c2856734add108a70f4f3d1c11817f9e
deepak852k1 <deepak852k1@gmail.com>
2022-09-25 10:29:32 +0530

    home done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './Header';
-
+import  Home  from  './Home' ;
+import Product from './Product';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
+      
     </div>
   );
 }
